e5233a5881f69e8033bba601895c467d5b76b0ed
Andrew Massey <andrewreaganm@ou.edu>
2019-10-28 20:20:49 -0500

    boilerplate code for handling data
    * Creates index based on name and timestamp.

--- testEnv/main.py
+++ testEnv/main.py
@@ -1,7 +1,21 @@
+# Sample for how to use the stockDataHandler
+
+import glob
+import os
 import pandas as pd
+import stockDataHandler as sdh
 
-print("Hello world")
 
-apple = pd.read_csv("/Users/andrewmassey/Documents/2019 - Fall/SoftwareEng/MSCI/price-volume-data-for-all-us-stocks"
-                    "-etfs/Stocks/aapl.us.txt")
-print(apple.head(5))
+# Path to Stock directory
+pathStocks = "/Users/andrewmassey/Documents/2019 - Fall/SoftwareEng/MSCI/price-volume-data-for-all-us-stocks-etfs" \
+             "/Stocks"
+# Path to ETF directory
+pathETFs = "/Users/andrewmassey/Documents/2019 - Fall/SoftwareEng/MSCI/price-volume-data-for-all-us-stocks-etfs/ETFs"
+
+# Create list of all filenames to be imported
+filenames = glob.glob(os.path.join(pathStocks, "*.txt"))  # Create list of Stock filenames.
+filenames += glob.glob(os.path.join(pathETFs, "*.txt"))  # Create list of ETF filenames
+
+# Import stocks and ETFs
+print("Importing Stocks and ETFs from .txt files....")
+new_df = sdh.stock_dataframe(filenames, print_sample=5, print_dtypes=True, verbose=True)
---
+++ testEnv/stockDataHandler.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+
+
+def import_stock_csv(_file_list, verbose=False):
+    _dfList = []
+    for file in _file_list:
+        _file_name = os.path.splitext(os.path.basename(file))[0]
+        try:
+            _dfn = pd.read_csv(file)
+            _objName = _file_name.split('.', 1)[0]
+            _dfn['Name'] = _objName
+            _dfn.index.name = _file_name
+            # Convert Date field to correct format.
+            _dfn['Date'] = pd.to_datetime(_dfn['Date'])
+            _dfList.append(_dfn)
+            # print("Imported " + file_name)
+        except Exception as e:
+            if verbose:
+                print(str(e) + ": '" + _file_name + ".txt' File may be blank.")
+    return _dfList
+
+
+def stock_dataframe(_file_list, verify_integrity=True, print_sample=0, print_dtypes=False, verbose=False):
+    dflist = import_stock_csv(_file_list, verbose=verbose)
+
+    if verbose:
+        print("Concatenating " + str(len(dflist)) + " dataframes.")
+
+    _dataframe = pd.concat(dflist, axis=0, ignore_index=True)
+    if verbose:
+        print("Setting up index and verifying index integrity...")
+
+    # Reindexes dataframe using name and date
+    _dataframe.set_index(['Name', 'Date'], inplace=True, verify_integrity=verify_integrity)
+
+    # Prints a sampling of data
+    if print_sample != 0:
+        print("Post-indexing sample:")
+        print(_dataframe.head(5))
+
+    if verbose:
+        print("Import complete\nLines: " + str(len(_dataframe.index)))
+
+    if print_dtypes:
+        print("Data Types:")
+        print(_dataframe.dtypes)
+
+    return _dataframe

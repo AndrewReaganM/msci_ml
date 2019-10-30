b6ee026d764a04345e781a197d688d31a914834e
Andrew Massey <andrewreaganm@ou.edu>
2019-10-29 22:58:53 -0500

    Updated import code
    * Switched from using a list to using a dictionary when importing as Dataframes.
    * Added a 'date_as_index' optional parameter in import_stock_csv. This allows for better indexing after concatenating multiple dataframes.
    * Added import_supp_data function to import data about the stocks as a dataframe indexed by ticker.
    * Added better documentation.

--- testEnv/main.py
+++ testEnv/main.py
@@ -1,4 +1,6 @@
-# Sample for how to use the stockDataHandler
+"""
+Example for how to use the stockDataHandler.
+"""
 
 import glob
 import os
@@ -12,10 +14,25 @@ pathStocks = "/Users/andrewmassey/Documents/2019 - Fall/SoftwareEng/MSCI/price-v
 # Path to ETF directory
 pathETFs = "/Users/andrewmassey/Documents/2019 - Fall/SoftwareEng/MSCI/price-volume-data-for-all-us-stocks-etfs/ETFs"
 
+# Path to additional data.
+comp_data = "/Users/andrewmassey/Documents/2019 - Fall/SoftwareEng/MSCI/supplementalData/all-us-stocks-tickers" \
+            "-company-info-logos/companies.csv"
+
+stock_file_ext = "*.txt"
 # Create list of all filenames to be imported
-filenames = glob.glob(os.path.join(pathStocks, "*.txt"))  # Create list of Stock filenames.
-filenames += glob.glob(os.path.join(pathETFs, "*.txt"))  # Create list of ETF filenames
+filenames = glob.glob(os.path.join(pathStocks, stock_file_ext))  # Create list of Stock filenames.
+filenames += glob.glob(os.path.join(pathETFs, stock_file_ext))  # Concatenate the list of ETF filenames
 
 # Import stocks and ETFs
-print("Importing Stocks and ETFs from .txt files....")
-new_df = sdh.stock_dataframe(filenames, print_sample=20, print_dtypes=True, verbose=True)
+print("Importing Stocks and ETFs from .txt files...")
+stock_df = sdh.stock_dataframe(filenames, print_sample=5, print_dtypes=False, verbose=False)
+
+# Import supplemental data
+print("Importing supplemental company metadata...")
+comp_df = sdh.import_supp_data(comp_data)
+
+print("Import complete.")
+
+# Example queries
+print(comp_df.query('ticker == "tsco"'))
+print(stock_df.query('Name == "tsco"'))
--- testEnv/stockDataHandler.py
+++ testEnv/stockDataHandler.py
@@ -1,5 +1,5 @@
 """
-Library for using a pandas dataframe to import stock/etf data from Kaggle.
+Library for using a Pandas dataframe to import stock/etf data from Kaggle.
 
 """
 
@@ -7,23 +7,31 @@ import os
 import pandas as pd
 
 
-def import_stock_csv(_file_list, verbose=False):
-    _dfList = []
+def import_stock_csv(_file_list, verbose=False, date_as_index=True):
+    """
+    Function to import .txt file contents into a dictionary of Pandas dataframes.
+
+    :param _file_list: List of stock data file paths.
+    :param verbose: Bool to print errors or not.
+    :return: dictionary of dataframes keyed by ticker.
+    """
+    _dfDict = {}
     for _file in _file_list:
         _file_name = os.path.splitext(os.path.basename(_file))[0]
         try:
-            _dfn = pd.read_csv(_file)
-            _objName = _file_name.split('.', 1)[0]
-            _dfn['Name'] = _objName
+            _dfn = pd.read_csv(_file, engine='c')  # Read in CSV
+            _objName = _file_name.split('.', 1)[0] # Retrieve the stock name
+            _dfn['Name'] = _objName # Add column with stock name
             _dfn.index.name = _file_name
-            # Convert Date field to correct format.
-            _dfn['Date'] = pd.to_datetime(_dfn['Date'])
-            _dfList.append(_dfn)
+            _dfn['Date'] = pd.to_datetime(_dfn['Date'])  # Convert Date field to correct data type.
+            if date_as_index:
+                _dfn.set_index(['Date'], inplace=True, verify_integrity=True)  # Set date as index if specified
+            _dfDict[_objName] = _dfn  # Add DF to dictionary
             # print("Imported " + file_name)
         except Exception as e:
-            if verbose:
+            if verbose: # Print exception
                 print(str(e) + ": '" + _file_name + ".txt' File may be blank.")
-    return _dfList
+    return _dfDict # Return a dictionary of dataframes keyed by ticker.
 
 
 def stock_dataframe(_file_list, verify_integrity=True, print_sample=0, print_dtypes=False, verbose=False):
@@ -38,28 +46,38 @@ def stock_dataframe(_file_list, verify_integrity=True, print_sample=0, print_dty
     :param verbose: bool: more verbose printing to console.
     :return: complete pandas dataframe.
     """
-    _dflist = import_stock_csv(_file_list, verbose=verbose)
+    _dflist = import_stock_csv(_file_list, verbose=verbose, date_as_index=False)
 
     if verbose:
         print("Concatenating " + str(len(_dflist)) + " dataframes.")
 
-    _dataframe = pd.concat(_dflist, axis=0, ignore_index=True)
+    _dataframe = pd.concat(_dflist, axis=0, ignore_index=True)  # Concatenate dataframes from dictionary/list
     if verbose:
         print("Setting up index and verifying index integrity...")
 
-    # Reindexes dataframe using name and date
-    _dataframe.set_index(['Name', 'Date'], inplace=True, verify_integrity=verify_integrity)
+    # Reindex dataframe using name and date
+    try:
+        _dataframe.set_index(['Name', 'Date'], inplace=True, verify_integrity=verify_integrity)
+    except KeyError as e:
+        print("Indexing error" + str(e) + ". Make sure that this column was not an index in the external DF.")
 
     # Prints a sampling of data
     if print_sample != 0:
-        print("Post-indexing sample:")
         print(_dataframe.head(print_sample))
 
     if verbose:
         print("Import complete\nLines: " + str(len(_dataframe.index)))
 
-    if print_dtypes:
+    if print_dtypes:  # Optionally print datatypes for verification.
         print("Data Types:")
         print(_dataframe.dtypes)
 
-    return _dataframe
+    return _dataframe # Return individual dataframe.
+
+
+def import_supp_data(_comp_data, verify_integrity=True):
+    _comp_df = pd.read_csv(_comp_data) # Read CSV
+    _comp_df['ticker'] = _comp_df['ticker'].str.lower() # Make ticker lowercase for easier access.
+    _comp_df.set_index(['ticker'], inplace=True, verify_integrity=verify_integrity) # Set ticker as index.
+    _comp_df.columns = [_c.replace(' ', '_') for _c in _comp_df.columns] # Replace spaces with underscores.
+    return _comp_df # Return individual dataframe.
